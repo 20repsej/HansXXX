31095a6245c03be27830c25343859eee6cbc819e
Jesper Berg <jesper.berg@mailbox.org>
2021-02-23 04:48:13 -0800

    mycket fint
    Co-authored-by: Ivar Jernberg <18jeriva@soltorgsgymnasiet.se>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([60.492695, 15.416517], 14);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([60.492695, 15.416517]).addTo(map)
+    .bindPopup('Soltorgsgymnasiet')
+    .openPopup();
+
+function search() {
+    query = 'hello world';
+    url = 'http://www.google.com/search?q=' + query;
+    window.open(url, '_blank');
+}
